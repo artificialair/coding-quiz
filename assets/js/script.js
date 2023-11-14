b1716a250e70eac9b779016e88cbbf6d3306efa6
artificialair <jaybaker964@gmail.com>
2023-11-14 17:07:30 -0500

    added highscores page, and it works :o

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,17 @@ var startScreen = document.querySelector('.start');
 var quizScreen = document.querySelector('.quiz');
 var endScreen = document.querySelector('.end');
 var scoresScreen = document.querySelector('.highscores');
+
 var startButton = document.querySelector('#start-button');
 var questionsArea = document.querySelector('.question-area');
 var viewHighscores = document.querySelector('#view-scores');
 var scoreDisplay = document.querySelector('#score');
-
 var initialsField = document.querySelector('#initials-field');
 var submitButton = document.querySelector('#submit-button');
+var highscoresList = document.querySelector('#highscore-list');
+
+var clearScoresButton = document.querySelector('#clear-scores-button');
+var playAgainButton = document.querySelector('#play-again-button');
 
 var highscores = [];
 
@@ -87,7 +91,26 @@ function decrementScore(amount) {
     }
 }
 
+// The following function renders items in the highscores list as <li> elements
+function renderHighscores() {
+    // Clear highscoresList element
+    highscoresList.innerHTML = "";
+  
+    // Render a new li for each highscore, including initials and score
+    highscores = highscores.sort(function(a, b){return b.score - a.score})
+    for (var i = 0; i < highscores.length; i++) {
+      var highscore = highscores[i];
+  
+      var li = document.createElement("li");
+      li.textContent = highscore.initials + " - " + highscore.score;
+      li.setAttribute("data-index", i);
+
+      highscoresList.appendChild(li);
+    }
+  }
+
 viewHighscores.addEventListener('click', function() {
+    // if the timer is active, stop it.
     if (timer) {
         clearInterval(timer);
     }
@@ -122,22 +145,38 @@ quizScreen.addEventListener('click', function(event) {
 submitButton.addEventListener('click', function(event) {
     event.preventDefault();
 
-    highscores += {
+    // add data from the forum and score to the local highscores list
+    highscores.push({
         "initials": initialsField.value,
         "score": score
-    }
+    });
 
-    console.log(highscores)
+    // clear text field
+    initialsField.value = "";
 
-    localStorage.setItem("highscores", JSON.stringify(highscores))
+    // update local storage and move to highscores page
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+    show('highscores');
+    renderHighscores();
 });
 
+clearScoresButton.addEventListener('click', function() {
+    highscores = [];
+    localStorage.removeItem('highscores');
+    renderHighscores();
+});
+
+playAgainButton.addEventListener('click', function() {
+    show('start');
+})
+
 function init() {
+    // fetch highscore data from local storage, if it exists.
     var storedHighscores = JSON.parse(localStorage.getItem("highscores"));
     if (storedHighscores !== null) {
         highscores = storedHighscores;
     }
-    
+
     show('start');
 }
 
